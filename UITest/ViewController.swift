3bf5454bb979073fba3059ebcc2b6ffb5bbcebb7
David Solano Ruiz <d.solano@gmail.com>
2016-08-07 21:31:03 +0200

    Initial Commit

---
+++ UITest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITest
+//
+//  Created by David Solano Ruiz on 7/8/16.
+//  Copyright © 2016 David Solano Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
