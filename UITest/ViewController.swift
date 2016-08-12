f81811876c4227911837bf8ffbf5d34cbe3633fb
David Solano Ruiz <d.solano@gmail.com>
2016-08-12 13:39:47 +0200

    Initial commit UI

--- UITest/ViewController.swift
+++ UITest/ViewController.swift
@@ -8,18 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Home"
+        
+        collectionView?.backgroundColor = UIColor.whiteColor()
+        collectionView?.registerClass(VideoCell.self, forCellWithReuseIdentifier: "cellId")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
-
-
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cellId", forIndexPath: indexPath)
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return CGSizeMake(view.frame.width, 350)
+    }
 }
+
+class VideoCell: UICollectionViewCell{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let thumbnailImageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.blueColor()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    func setupViews(){
+         backgroundColor = UIColor.redColor()
+        
+        addSubview(thumbnailImageView)
+        addConstraintsWithFormat("H:|-16-[v0]-16-|", views: thumbnailImageView )
+        addConstraintsWithFormat("V:|-16-[v0]-16-|", views: thumbnailImageView )
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+extension UIView{
+    func addConstraintsWithFormat(format:String, views:UIView...){
+        var viewsDictionary = [String:UIView]()
+        for (index, view) in views.enumerate(){
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
+
+
+
+
+
 
